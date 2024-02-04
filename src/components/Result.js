e8470213ce6335b60c2796a530c546ebc83b2506
MasterPieceSVK <112313437+MasterPieceSVK@users.noreply.github.com>
2024-02-04 20:56:49 +0100

    black color

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,7 +1,7 @@
 export default function Result({ result, getDeparturesByStopId }) {
   return (
     <button
-      className="btn btn-outline btn-secondary"
+      className="btn btn-outline btn-neutral"
       value={result.stop_id}
       onClick={getDeparturesByStopId}
     >
