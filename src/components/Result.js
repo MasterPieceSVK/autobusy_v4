f68fb67df7284b7cde1e6b43a2b4d3ccba170b90
MasterPieceSVK <112313437+MasterPieceSVK@users.noreply.github.com>
2024-02-05 22:04:53 +0100

    only positive results in search

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,11 +1,13 @@
 export default function Result({ result, getDeparturesByStopId }) {
-  return (
-    <button
-      className="btn btn-outline btn-neutral"
-      value={result.stop_id}
-      onClick={getDeparturesByStopId}
-    >
-      {result.stop_name}
-    </button>
-  );
+  if (result.stop_id > 0) {
+    return (
+      <button
+        className="btn btn-outline btn-neutral"
+        value={result.stop_id}
+        onClick={getDeparturesByStopId}
+      >
+        {result.stop_name}
+      </button>
+    );
+  }
 }
