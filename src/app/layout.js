a94b3ee5fb5530d1ad497369d5ba22279142c730
MasterPieceSVK <112313437+MasterPieceSVK@users.noreply.github.com>
2024-02-09 19:34:29 +0100

    ga

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/Nav";
-
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -13,6 +13,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-E7NY2W59JZ"
+        />
+
+        <Script id="google-analytics">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-TNV5ZPSTFW');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <div>
           <Nav></Nav>
